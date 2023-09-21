c7143541bb72f6694ee7f77424acebac38ac8e7b
Dinesh Kumar <dyyayavar7491@gmail.com>
2023-09-21 17:38:07 +0530

    Rapid Api fetch

---
+++ utils/index.ts
@@ -0,0 +1,16 @@
+
+export async function fetchCars() {
+    const headers = {
+        headers: {
+            'X-RapidAPI-Key': '6f0289d155msha70bfa2ae781d40p117d36jsnfc6776e00755',
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+        }
+    }
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
+}
